42e331cfdb935f9a54c19242f3f7704179803c8e
Svetlana <mayseventeenth@mail.ru>
2017-08-30 22:04:22 +0300

    renamed fs + add styles + add new file: gulpfile.js and gulp packages to packages.json

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var postcss_simple_vars = require('postcss-simple-vars');
+var postcss_nested = require('postcss-nested');
+
+
+
+// Styles Task
+gulp.task('styles', function(){
+	return gulp.src('./app/src/styles/styles.css')
+	   .pipe(postcss([postcss_nested, postcss_simple_vars, autoprefixer]))
+       .pipe(gulp.dest('./app/build/styles'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function(){
+    gulp.watch('./app/src/styles/**/*.css', ['styles']);
+});
+
+
+// Default Task
+gulp.task('default');
