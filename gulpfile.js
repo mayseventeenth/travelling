530a6a6dd8f2cb03903ec651d47c473eaac0cc2d
Svetlana <mayseventeenth@mail.ru>
2017-08-31 09:24:15 +0300

    fs:styles: add global and modules folders; modified:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var postcss_simple_vars = require('postcss-simple-vars');
 var postcss_nested = require('postcss-nested');
-
+var postcss_import = require('postcss-import');
 
 
 // Styles Task
 gulp.task('styles', function(){
 	return gulp.src('./app/src/styles/styles.css')
-	   .pipe(postcss([postcss_nested, postcss_simple_vars, autoprefixer]))
+	   .pipe(postcss([postcss_import, postcss_nested, postcss_simple_vars, autoprefixer]))
        .pipe(gulp.dest('./app/build/styles'));
 });
 
