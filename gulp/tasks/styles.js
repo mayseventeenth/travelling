576e7574ab7f606c7481af62fe96e2d7defdba0e
Svetlana <mayseventeenth@mail.ru>
2017-09-21 15:05:19 +0300

    Completed header with menu

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,11 +9,12 @@ var postcss_simple_vars = require('postcss-simple-vars');
 var postcss_nested = require('postcss-nested');
 var postcss_import = require('postcss-import');
 var postcss_mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 // Styles Task
 gulp.task('styles', function(){
 	return gulp.src('./app/src/styles/styles.css')
-	   .pipe(postcss([postcss_import, postcss_mixins, postcss_nested, postcss_simple_vars, autoprefixer]))
+	   .pipe(postcss([postcss_import, postcss_mixins, postcss_nested, postcss_simple_vars, hexrgba, autoprefixer]))
       	.on('error', function(errorInfo){
 			console.log(errorInfo.toString());
 			this.emit('end');
